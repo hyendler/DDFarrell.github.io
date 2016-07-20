f93c08d4474fc2e8cba7be3bd18e851ee6f85258
DDFarrell <DD.Farrell82@gmail.com>
2016-07-20 15:46:43 -0700

    tried to change pygame pictures. didnt work

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+	$("game").click(function(){
+		$(this).attr("src", "http://4.bp.blogspot.com/_AuTEvUYi4Ms/TTt7IuU0M4I/AAAAAAAABPk/xpiLcRZ7v5E/s1600/Snow_With_Black_Background.png")
+	});
+})
