b6a74fc038823d0e8c33ee5e9ccf6379354aa0f5
DDFarrell <DD.Farrell82@gmail.com>
2016-07-21 10:38:36 -0700

    tried to add jquery but nothing changed

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
-	$("game").click(function(){
-		$(this).attr("src", "http://4.bp.blogspot.com/_AuTEvUYi4Ms/TTt7IuU0M4I/AAAAAAAABPk/xpiLcRZ7v5E/s1600/Snow_With_Black_Background.png")
-	});
-})
+$('#game').click(function(){
+    $(this).attr("src","images/snow-scene.png");
+});
+
+});
